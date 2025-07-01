32b5d2994e618eeb67636775fecbc9f6760f9973
xkyota <mdurihin@gmail.com>
2025-07-01 14:56:48 +0300

    quick classes names fix

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ for (let index = 0; index < 12; index++) {
         <li>
 				<input type="checkbox" name="checkbox" class="delete-checkbox" />
 				<span class="model">JVSC12312321</span>
-				<span class="name">ACME DISC</span>
+				<span class="type">ACME DISC</span>
 				<span class="price">1.00$</span>
-				<span class="size">Size: 700MB</span>
+				<span class="addon">Size: 700MB</span>
 		</li>
         `
 	);
